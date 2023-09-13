58cfb9fb1236c14118fcca4b5a72923062b0da34
garrettallen0 <garrett.allen13@gmail.com>
2023-09-13 13:30:33 -0700

    styles button menu

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -6,11 +6,11 @@ import React from 'react';
 const Menu = () => {
 
     return (
-       <>
+       <div className='menu'>
     <h2>button 1</h2>
     <h2>button 2</h2>
     <h2>button 3</h2>
-    </>
+    </div>
     )
 }
 
