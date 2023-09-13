5b3b9777887bbc92200a2312b99cb752b3fd7cb1
garrettallen0 <garrett.allen13@gmail.com>
2023-09-13 14:03:01 -0700

    adds bird data

--- components/App.jsx
+++ components/App.jsx
@@ -5,6 +5,24 @@ import Menu from './Menu.jsx'
 
 export default function App() {
 
+  const [speciesList, setSpeciesList] = useState([])
+    
+  useEffect(() => {
+    fetch('https://api.ebird.org/v2/data/obs/US-CA-001/recent', {
+      headers: {"x-ebirdapitoken": "k35m7c131mkp"}})
+      .then(res => res.json())
+      .then(res => {
+
+        const list = res.map(item => item.comName);
+        setSpeciesList(list);
+        
+      })
+      .catch(err => console.log('App.componentDidMount: get species data: ERROR: ', err));
+
+  }, []);
+
+  console.log('species list', speciesList)
+
 return (
     <> 
     <Board />
